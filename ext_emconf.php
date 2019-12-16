cdec2856fbbe03c23481f0fabfd29cf88272dd0d
Annika Reußner <annika.reussner@hs-furtwangen.de>
2019-12-16 17:05:47 +0100

    TASK: hfu_opus Kompatiblität zu Typo3 8
    Ab Version 9.0 wurde die ViewHelper-Classe AbstractViewHelper aus dem
    Core entfernt. Aus diesem Grund ändert sich der Namespace der
    verwendeten Klasse zu TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper,
    sowie die Definition der Attribute.
    
    hfu_opus ist deswegen nur kompatibel mit Typo3 Version 9 oder höher.
    
    Info - Changelog 9.0 Changes:
    - Breaking: #82414 - CMS ViewHelper base classes removed
    
    Refs: #WEB-82

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7-9.5.99',
+            'typo3' => '9.0.0 - 9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
